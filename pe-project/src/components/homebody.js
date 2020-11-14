47312c0bae595499c45ae91bcf874fb62e00be1a
Koolmale <kumel.h61110@gmail.com>
2020-11-13 21:24:28 -0800

    home pg

--- pe-project/src/components/homebody.js
+++ pe-project/src/components/homebody.js
@@ -1,5 +1,6 @@
 import { requirePropFactory, Button } from '@material-ui/core'
 import React from 'react'
+import StickyFooter from 'react-sticky-footer';
 import '../styles/homebody.css'
 
 function Homebody() {
@@ -24,15 +25,45 @@ function Homebody() {
 				</div>
 
 				<div id='section3'>
-					<div id='section3img'>
-						<img src='https://res.cloudinary.com/grohealth/image/upload/f_auto,fl_lossy,q_auto/v1581678662/DCUK/Content/iStock-959080376.jpg' alt='basketball'/>
-					</div>
 					<div id='section3txt'>
-						<h3>Basketball</h3>
+						<h2>Basketball</h2>
 						<Button variant="contained" size="large" color="primary" className='button'>
 						Tutorials
 						</Button>
 					</div>
+				</div>
+
+				<div id='section4'>
+					<div>
+						<h2>Flag Football</h2>
+						<Button variant="contained" size="large" color="primary" className='button'>
+							Tutorials
+						</Button>
+					</div>
+				</div>
+
+				<div id='section5'>
+				<h2>Strength Training</h2>
+				<Button variant="contained" size="large" color="primary" className='button'>
+					Tutorials
+				</Button>
+				</div>
+
+
+				<div>
+				<StickyFooter
+					bottomThreshold={50}
+					normalStyles={{
+					backgroundColor: " #53a54881",
+					padding: "2rem"
+					}}
+					stickyStyles={{
+					backgroundColor: "rgba(255,255,255,.8)",
+					padding: "2rem"
+					}}
+				>
+					Designed By Kazaa Sports &copy;
+				</StickyFooter>
 				</div>
 			</div>
 		</div>
