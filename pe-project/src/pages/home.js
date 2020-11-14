7214613bfe0783285824ffcf3095ca443cc91fbf
Koolmale <kumel.h61110@gmail.com>
2020-11-13 21:46:26 -0800

    dev branch

---
+++ pe-project/src/pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../components/nav'
+import Homebody from '../components/homebody'
+import StrengthTraning from '../components/strength_training'
+
+function Home() {
+	return (
+		<div>
+			<Nav />
+			<Homebody />
+		</div>
+	)
+}
+
+export default Home
